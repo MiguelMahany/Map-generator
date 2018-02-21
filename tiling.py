e57b02eb265f7c7b0484951c1c8fdbba2563ae44
Miguel Mahany <miguelmahany@gmail.com>
2018-02-20 20:46:03 -0800

    Actually draws now yay

--- tiling.py
+++ tiling.py
@@ -1,17 +1,19 @@
 from PIL import Image,ImageDraw
 
 origin = [0,0]
-resolution = [100,100]
+resolution = [1920,1080]
 image = Image.new('RGB',resolution,(255,255,255))
+image.save("test.png","PNG")
 
 
 def tile_Go(origin,res,gridres):
     res = res
     tile_size = gridres
-    if origin >= res:
-        return print("Done")
+    tile_size = 10
+    if origin[0]>=res[0]:
+        return True
     else:
-        if res[0]<=origin[0]&res[1]&res[1]:
+        if origin[0] >= res[0]:
             origin[0]=0
             origin[1]+=-4*tile_size
         else:
@@ -19,11 +21,11 @@ def tile_Go(origin,res,gridres):
             x=origin[0]
             y=origin[1]
             a = origin
-            b = [1*tile_size,-2*tile_size]
-            c = [x,-4*tile_size]
+            b = [1*tile_size,2*tile_size]
+            c = [x,4*tile_size]
             d = [2*tile_size,y]
-            e = [2*tile_size,-4*tile_size]
-            f = [4*tile_size,-4*tile_size]
+            e = [2*tile_size,4*tile_size]
+            f = [4*tile_size,4*tile_size]
             g = [4*tile_size,y]
             pointa = (origin[0]),(origin[1])
             pointb = (origin[0]+b[0]), (origin[1]+b[1])
@@ -33,12 +35,17 @@ def tile_Go(origin,res,gridres):
             pointf = (origin[0]+f[0]), (origin[1]+f[1])
             pointg = (origin[0]+g[0]), (origin[1]+g[1])
             
+            im = Image.open("test.png")
+            
 
-            lines = (pointa,pointb),(pointb,pointc),(pointb,pointd),(pointb,pointe),(pointd,pointg),(pointe,pointf)
+            lines = [pointa,pointb,pointb,pointc,pointb,pointd,pointb,pointe,pointd,pointg,pointe,pointf]
 
-            draw = ImageDraw.Draw(image)
-            draw.line(lines)
-            image.save("test.png","PNG")
+            draw = ImageDraw.Draw(im)
+            draw.line(lines,fill="Black", width=1)
+            print(pointa)
+            print(pointg)
+            origin = pointg
+            im.save("test.png","PNG")
             tile_Go(origin,res,gridres)
             # fix it yo #
 
