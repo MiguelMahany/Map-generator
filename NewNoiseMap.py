aa298b5acfdec2fb0a26526ae24b802d0041e43b
Miguel Mahany <miguelmahany@gmail.com>
2018-03-28 13:32:11 -0700

    Cleaned up, added default values, new check specifically for SCALE var

--- NewNoiseMap.py
+++ NewNoiseMap.py
@@ -7,25 +7,36 @@ class NoiseMapGenerator:
     def __init__(self,WIDTH,HEIGHT,SCALE,OCTAVES,PERSISTANCE,LACUNARITY):
         self.WIDTH = int(WIDTH)
         self.HEIGHT = int(HEIGHT)
-        self.SCALE = int(SCALE)
+        if int(SCALE) <=0:
+            NEWSCALE = 1
+        self.SCALE = 1/int(NEWSCALE)
         self.OCTAVES = int(OCTAVES)
         self.PERSISTANCE = float(PERSISTANCE)
         self.LACUNARITY = float(LACUNARITY)
-        self.noisemap = [[0 for i in range (self.WIDTH)] for j in range(self.HEIGHT)]
         
 
     def StartMap(self):
-        image = Image.new('RGB',(self.HEIGHT,self.WIDTH),'White')
-        if self.SCALE <=0:
-            self.SCALE = 0.0001
+        if self.SCALE <=1:
+            self.SCALE = 1/28
         if self.WIDTH <=0:
-            self.WIDTH = 1
+            self.WIDTH = 500
         if self.HEIGHT <= 0:
-            self.HEIGHT = 1
+            self.HEIGHT = 500
+        if self.OCTAVES <=0:
+            self.OCTAVES = 3
+        if self.PERSISTANCE <=0:
+            self.PERSISTANCE = .5
+        if self.LACUNARITY <=0:
+            self.LACUNARITY = 2
+        
+        self.noisemap = [[0 for i in range (self.WIDTH)] for j in range(self.HEIGHT)]
+
+        image = Image.new('RGB',(self.HEIGHT,self.WIDTH),'White')
 
         for y in range(0,self.HEIGHT):
             for x in range(0,self.WIDTH):
-                self.noisemap[x][y] = pnoise2(x*.07,y*.07,self.OCTAVES,self.PERSISTANCE,self.LACUNARITY,1024,1024,3)
+                self.noisemap[x][y] = pnoise2(x*self.SCALE,y*self.SCALE,self.OCTAVES,self.PERSISTANCE,self.LACUNARITY,1024,1024,0)
+
         draw = ImageDraw.Draw(image)
         for y in range(0,self.HEIGHT):
             for x in range(0,self.WIDTH):
@@ -36,6 +47,5 @@ class NoiseMapGenerator:
                 elif color < 0:
                     color= 0
                 draw.point((x,y),fill = (color,color,color,255))
-
         image.save("NoiseMap.png")
         return True
